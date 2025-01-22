f92fdf21bc8f22a7078417b3c937a7a078a85259
Ser090 <programacion.software.00@gmail.com>
2025-01-22 18:08:42 +0100

    Empezado la ventana de ArticulosDetalle

--- src/crud/iu/controladores/ControladorArticulosDetalle.java
+++ src/crud/iu/controladores/ControladorArticulosDetalle.java
@@ -4,24 +4,27 @@ import crud.negocio.FactoriaAlmacen;
 import crud.negocio.FactoriaArticulos;
 import crud.objetosTransferibles.Almacen;
 import crud.objetosTransferibles.Articulo;
-import crud.objetosTransferibles.Estado;
-import crud.objetosTransferibles.Pedido;
 import crud.objetosTransferibles.Trabajador;
-import crud.objetosTransferibles.Usuario;
 import java.net.URL;
-import java.util.Date;
+import java.time.ZoneId;
+import java.util.Collection;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.Pagination;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -31,13 +34,13 @@ import javafx.stage.Stage;
 public class ControladorArticulosDetalle implements Initializable {
 
     private static final Logger LOGGER = Logger.getLogger(ControladorArticulosDetalle.class.getName());
-    private FactoriaArticulos factoriaPedidos = FactoriaArticulos.getInstance();
+    private FactoriaArticulos factoriaArticulos = FactoriaArticulos.getInstance();
     private FactoriaAlmacen factoriaAlmacenes = FactoriaAlmacen.getInstance();
     private Stage stage;
-    private Usuario usuario;
-    private ObservableList<Articulo> articulosObservableList;
-    private ObservableList<Almacen> almacenesObservableList;
-    private static final int FILAS_POR_PAGINA = 14;
+    private Articulo articulo;
+    private Trabajador userTrabajador;
+    private ObservableList<Articulo> articulos;
+    private ObservableList<Almacen> almacenesDisponibles;
 
     // Elementos FXML
     @FXML
@@ -50,31 +53,28 @@ public class ControladorArticulosDetalle implements Initializable {
     private Button botonBusqueda;
     @FXML
     private Button botonEliminar;
+
     @FXML
-    private Button botonGuardar;
+    private TableView<Almacen> tablaAlmacenesDisponibles;
     @FXML
-    private Button botonDetalles;
+    private TableColumn<Almacen, Long> columnaId;
     @FXML
-    private Button botonAtras;
+    private TableColumn<Almacen, String> columnaDireccion;
     @FXML
-    private TableView<Pedido> tablaPedidos;
+    private TableColumn<Almacen, String> columnaEspacios;
+
     @FXML
-    private TableColumn<Pedido, Long> columnaId;
+    private TextField campoId;
     @FXML
-    private TableColumn<Pedido, Long> columnaUsuarioId;
+    private TextField campoNombre;
     @FXML
-    private TableColumn<Pedido, String> columnaCif;
+    private TextField campoPrecio;
     @FXML
-    private TableColumn<Pedido, Estado> columnaEstado;
+    private DatePicker campoFecha;
     @FXML
-    private TableColumn<Pedido, Date> columnaFecha;
+    private TextField campoDescripcion;
     @FXML
-    private TableColumn<Pedido, Double> columnaTotal;
-    @FXML
-    private Pagination paginador;
-    @FXML
-    private MenuItem opcionIrPedidos;
-    private Trabajador userTrabajador;
+    private TextField campoStock;
 
     // Copia de seguridad de los datos originales
     private ObservableList<Articulo> articulosOriginales;
@@ -88,11 +88,28 @@ public class ControladorArticulosDetalle implements Initializable {
         LOGGER.info("Inicializando la escena principal");
         stage.show();  // Mostrar el escenario
 
+        if (articulo != null) {
+            campoId.setText(String.valueOf(articulo.getId()));
+            campoNombre.setText(articulo.getNombre());
+            campoPrecio.setText(articulo.getPrecio() + "€");
+            campoDescripcion.setText(articulo.getDescripcion());
+            campoStock.setText(articulo.getStock() + "unid.");
+
+            campoFecha.setValue(
+                    articulo.getFechaReposicion().toInstant()
+                            .atZone(ZoneId.systemDefault())
+                            .toLocalDate()
+            );
+
+        }
+
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         LOGGER.info("Inicializando controlador ArticulosDetalle");
+        configurarTablas();
+        cargarAlmacenesDisponibles();
 
     }
 
@@ -107,6 +124,56 @@ public class ControladorArticulosDetalle implements Initializable {
     public void setStage(Stage stage) {
         this.stage = stage;
         LOGGER.info("Stage asignado.");
+    }
+
+    private void configurarTablas() {
+
+        columnaId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnaDireccion.setCellValueFactory(new PropertyValueFactory<>("direccion"));
+        columnaEspacios.setCellValueFactory(new PropertyValueFactory<>("espacio"));
+
+        tablaAlmacenesDisponibles.getColumns().forEach(col -> col.setSortable(false));
+    }
+
+    private void cargarAlmacenesDisponibles() {
+        try {
+            Collection<Almacen> almacenes = factoriaAlmacenes.acceso().getAllAlmacenes();
+            almacenesDisponibles = FXCollections.observableArrayList(almacenes);
+
+            tablaAlmacenesDisponibles.setItems(almacenesDisponibles);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error al cargar los alamcenes disponibles", e);
+            mostrarAlerta(Alert.AlertType.ERROR, "Error", "No se pudieron cargar los almacenes disponibles.");
+
+        }
+    }
+
+    @FXML
+    private void handleAsignarAlmacen(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleEliminar(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void handleAtras(ActionEvent event) {
+        factoriaArticulos.cargarArticulosPrincipal(stage, userTrabajador);
+    }
+
+    @FXML
+    private void handleGuardarCambios(ActionEvent event) {
+        LOGGER.info("Botón Guardar Cambios presionado");
+
+    }
+
+    private void mostrarAlerta(Alert.AlertType tipo, String titulo, String mensaje) {
+        Alert alerta = new Alert(tipo);
+        alerta.setTitle(titulo);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
     }
 
 }
