cb8103c548a0ee5b03a7750687f70360a1f34973
Sergio <programacion.software.00@gmail.com>
2025-02-06 11:39:46 +0100

    Javadoc de negocio 2

--- src/crud/negocio/AlmacenImpl.java
+++ src/crud/negocio/AlmacenImpl.java
@@ -5,7 +5,6 @@
  */
 package crud.negocio;
 
-import crud.excepciones.LogicaNegocioException;
 import crud.objetosTransferibles.Almacen;
 import crud.rest.AlmacenRestFull;
 import java.util.Collection;
@@ -15,19 +14,43 @@ import java.util.logging.Logger;
 import javax.ws.rs.core.GenericType;
 
 /**
+ * Implementación de la interfaz {@link IAlmacen} para la gestión de almacenes.
+ * <p>
+ * Esta clase utiliza un cliente RESTful ({@link AlmacenRestFull}) para realizar
+ * operaciones de consulta y modificación sobre los almacenes.
+ * </p>
  *
- * @author Ser_090
+ * @author Sergio
  */
 public class AlmacenImpl implements IAlmacen {
 
+    /**
+     * Logger para la clase {@code AlmacenImpl}.
+     */
     private static final Logger LOGGER = Logger.getLogger(AlmacenImpl.class.getName());
 
+    /**
+     * Cliente RESTful para la comunicación con el servicio de almacenes.
+     */
     private AlmacenRestFull cliente;
 
+    /**
+     * Constructor que inicializa el cliente RESTful para almacenes.
+     */
     public AlmacenImpl() {
         cliente = new AlmacenRestFull();
     }
 
+    /**
+     * Obtiene todos los almacenes.
+     * <p>
+     * Realiza una consulta al servicio RESTful para obtener una colección de
+     * almacenes en formato XML.
+     * </p>
+     *
+     * @return Una colección de objetos {@link Almacen}.
+     * @throws Exception Si ocurre algún error durante la consulta.
+     */
     @Override
     public Collection<Almacen> getAllAlmacenes() throws Exception {
         List<Almacen> almacenes = null;
@@ -39,6 +62,18 @@ public class AlmacenImpl implements IAlmacen {
         return almacenes;
     }
 
+    /**
+     * Obtiene los almacenes asociados a un identificador específico.
+     * <p>
+     * Realiza una consulta al servicio RESTful para obtener una colección de
+     * almacenes filtrados por su identificador.
+     * </p>
+     *
+     * @param id El identificador para filtrar los almacenes.
+     * @return Una colección de objetos {@link Almacen} que corresponden al
+     * identificador dado.
+     * @throws Exception Si ocurre algún error durante la consulta.
+     */
     @Override
     public Collection<Almacen> getAllAlmacenesById(Long id) throws Exception {
         List<Almacen> almacenes;
@@ -50,6 +85,17 @@ public class AlmacenImpl implements IAlmacen {
         return almacenes;
     }
 
+    /**
+     * Crea o actualiza la relación de un almacén.
+     * <p>
+     * Este método envía un objeto {@link Almacen} al servicio RESTful para
+     * crear o actualizar su relación.
+     * </p>
+     *
+     * @param almacen El objeto {@link Almacen} que contiene la información a
+     * crear o actualizar.
+     * @throws Exception Si ocurre algún error durante la operación.
+     */
     @Override
     public void CrearActualizarRelacion(Almacen almacen) throws Exception {
 
@@ -57,6 +103,16 @@ public class AlmacenImpl implements IAlmacen {
         cliente.createRelacion_XML(almacen);
     }
 
+    /**
+     * Borra la relación de un almacén.
+     * <p>
+     * Este método envía un objeto {@link Almacen} al servicio RESTful para
+     * eliminar la relación existente.
+     * </p>
+     *
+     * @param almacen El objeto {@link Almacen} cuya relación se desea eliminar.
+     * @throws Exception Si ocurre algún error durante la operación.
+     */
     @Override
     public void BorrarRelacion(Almacen almacen) throws Exception {
         LOGGER.log(Level.INFO, "Estoy en la Implementacion {0}", almacen.getId());
--- src/crud/negocio/ArticuloImpl.java
+++ src/crud/negocio/ArticuloImpl.java
@@ -5,29 +5,52 @@
  */
 package crud.negocio;
 
-import crud.excepciones.LogicaNegocioException;
 import crud.objetosTransferibles.Articulo;
 import crud.rest.ArticulosRestFull;
 import java.util.Collection;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.core.GenericType;
 
 /**
+ * Implementación de la interfaz {@link IArticulo} para la gestión de artículos.
+ * <p>
+ * Esta clase utiliza un cliente RESTful ({@link ArticulosRestFull}) para
+ * realizar operaciones de consulta, creación, actualización y borrado de
+ * artículos.
+ * </p>
  *
- * @author 2dam
+ * @author Sergio
  */
 public class ArticuloImpl implements IArticulo {
 
+    /**
+     * Logger para la clase {@code ArticuloImpl}.
+     */
     private static final Logger LOGGER = Logger.getLogger(ArticuloImpl.class.getName());
 
+    /**
+     * Cliente RESTful para la comunicación con el servicio de artículos.
+     */
     private ArticulosRestFull cliente;
 
+    /**
+     * Constructor que inicializa el cliente RESTful para artículos.
+     */
     public ArticuloImpl() {
         cliente = new ArticulosRestFull();
     }
 
+    /**
+     * Obtiene todos los artículos disponibles.
+     * <p>
+     * Este método realiza una consulta al servicio RESTful para recuperar una
+     * colección de artículos en formato XML.
+     * </p>
+     *
+     * @return Una colección de objetos {@link Articulo}.
+     * @throws Exception Si ocurre algún error durante la consulta.
+     */
     @Override
     public Collection<Articulo> getAllArticulos() throws Exception {
         List<Articulo> articulos = null;
@@ -36,23 +59,62 @@ public class ArticuloImpl implements IArticulo {
         return articulos;
     }
 
+    /**
+     * Crea un nuevo artículo.
+     * <p>
+     * Este método envía un objeto {@link Articulo} al servicio RESTful para ser
+     * creado.
+     * </p>
+     *
+     * @param articulo El objeto {@link Articulo} que se desea crear.
+     * @throws Exception Si ocurre algún error durante la creación.
+     */
     @Override
-
     public void crearArticulo(Articulo articulo) throws Exception {
         cliente.create_XML(articulo);
     }
 
+    /**
+     * Actualiza un artículo existente.
+     * <p>
+     * Este método envía un objeto {@link Articulo} al servicio RESTful para
+     * actualizar la información del artículo.
+     * </p>
+     *
+     * @param articulo El objeto {@link Articulo} con los datos actualizados.
+     * @throws Exception Si ocurre algún error durante la actualización.
+     */
     @Override
     public void actualizarArticulo(Articulo articulo) throws Exception {
         cliente.edit_XML(articulo);
     }
 
+    /**
+     * Actualiza el detalle de un artículo.
+     * <p>
+     * Este método envía un objeto {@link Articulo} al servicio RESTful para
+     * actualizar únicamente los detalles del artículo.
+     * </p>
+     *
+     * @param articulo El objeto {@link Articulo} con los detalles actualizados.
+     * @throws Exception Si ocurre algún error durante la actualización del
+     * detalle.
+     */
     @Override
     public void actualizarArticuloDetalle(Articulo articulo) throws Exception {
-
         cliente.editDetalle_XML(articulo);
     }
 
+    /**
+     * Borra un artículo.
+     * <p>
+     * Este método envía el identificador del objeto {@link Articulo} al
+     * servicio RESTful para eliminar el artículo correspondiente.
+     * </p>
+     *
+     * @param articulo El objeto {@link Articulo} que se desea borrar.
+     * @throws Exception Si ocurre algún error durante el borrado.
+     */
     @Override
     public void borrarArticulo(Articulo articulo) throws Exception {
         cliente.remove(articulo.getId());
